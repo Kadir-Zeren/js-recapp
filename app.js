32966c7234dc7079230678f5caada9381baffb02
kadir-zeren <k4dzeren@gmail.com>
2024-02-12 23:21:04 +0300

    end

---
+++ app.js
@@ -0,0 +1,49 @@
+// console.log("localde bulanan js calisti");
+
+// alert(`bu bir alert dir`);
+
+// karar = confirm(`emin misiniz E/H`);
+// console.log(karar);
+
+// console.warn(`bu bir uyaridir`);
+
+// console.error(`Eyvah hata olustu..`);
+
+console.log(`app js running`);
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = `3`;
+console.log(typeof sayi1);
+
+const number1 = 5;
+
+const varMi = true;
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = `java`;
+console.log(language, typeof language);
+
+language = `Javascript`;
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
+
+adet = `23`;
+console.log(typeof adet);
+
+{
+  let localDegisken = 5;
+  localDegisken = localDegisken + 1;
+  console.log(localDegisken);
+}
